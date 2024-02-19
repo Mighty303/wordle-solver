16c64cacb34db259d1efc7eb19a2fff479c2d020
Mightyy <59546774+Mighty303@users.noreply.github.com>
2024-02-18 16:13:19 -0800

    added absent

--- wordle.go
+++ wordle.go
@@ -32,6 +32,7 @@ func FilterWords(words []string, correct string, valid string, absent string) []
 	// For example, if the feedback indicates that the letter 'a' is in the correct position 1,
 	// you would filter the list to include only words with 'a' in position 1.
 	correctPositions := strings.Split(correct, "")
+	absentPositions := strings.Split(absent, "")
 
 	// Eliminate non correct words
 	for _, word := range words {
@@ -43,7 +44,13 @@ func FilterWords(words []string, correct string, valid string, absent string) []
 					match = false
 				}
 			}
-			
+
+			for i := 0; i < len(absentPositions); i++ {
+				if strings.Contains(word, strings.ToLower(absentPositions[i])) {
+					match = false
+				}
+			}
+
 			if match {
 				newWords = append(newWords, word)
 				fmt.Printf("%s\n", word)
@@ -84,7 +91,7 @@ func main() {
 
 	correct = "A..L."
 	valid = "PE"
-	absent = "DVBMN"
+	absent = "DVBMNZX"
 
 	words = FilterWords(words, correct, valid, absent)
 	suggestion := SuggestWord(words)
